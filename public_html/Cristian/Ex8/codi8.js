c2be15fdcfa9698db197bdc1bc55202688c520e9
fgrange <fgrange@DESKTOP-IIQ0UG2>
2018-10-17 15:42:03 +0200

    Reorganització arxius 3

--- public_html/Cristian/Ex8/codi8.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-var hores = prompt("Introdueix un nombre d'hores");
-		alert("Setmanes" + parseInt(hores/168) + "\nDies: " + (7*((hores/168)-parseInt(hores/168))));
-
