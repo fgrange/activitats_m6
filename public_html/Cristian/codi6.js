6cfaefe1ff64c26eb27c14312e211cbea7c70c24
fgrange <fgrange@DESKTOP-IIQ0UG2>
2018-10-17 15:14:57 +0200

    Reorganització arxius

--- public_html/Cristian/codi6.js
+++ public_html/Cristian/codi6.js
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var num = 12;   //variable amb el nombre del que volem saber la taula de multiplicar
+const NUM = 12;   //variable amb el nombre del que volem saber la taula de multiplicar
 var resultat = "";  //variable que emmagatzemarà els diferents resultats de les multiplicacions
 for (var i = 0; i <= 10; i++) {    //bucle que iterarà 10 vegades
-    resultat += i + " * " + num + " =" + (parseInt(num)*parseInt(i)) + "\n";    //càlcul de la multiplicació per a cada valor de la i
+    resultat += i + " * " + NUM + " =" + (parseInt(NUM)*parseInt(i)) + "\n";    //càlcul de la multiplicació per a cada valor de la i
 }
 alert(resultat);    //mostrem per pantalla elresultat
