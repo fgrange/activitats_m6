0b67326244d4c9de3c42cb58c30be75265022971
fgrange <fgrange@DESKTOP-IIQ0UG2>
2018-10-25 16:22:03 +0200

    Exemple del funcionament de CANVAS per a poder fer la pràctica.

--- public_html/UF1_Sintaxis_i_objectes/02_ControlDeFlux/Ex16/script.js
+++ public_html/UF1_Sintaxis_i_objectes/02_ControlDeFlux/Ex16/script.js
@@ -3,16 +3,15 @@ function myFunction() {
 	var n=0;
 	for (var i=1 ; i<=5 ; i++){
 		do{
-			var sueldo = parseInt( prompt("Introduce el sueldo del empleado "+ i +"(1000€-3000€):") );
+			var sueldo = parseInt( prompt("Introduce el sueldo del empleado "+
+                                i +"(1000€-3000€):") );
 		}while(sueldo<1000 || sueldo>3000);
 
 		total=total+sueldo;
-		n=contar(sueldo, n);
+                if (sueldo<=2000) { n++;
+                }else{return n;}
 	}
-	alert("Hay "+ n +" empleados que cobran entre 1000€-2000€ y "+ (5-n) +" que cobran mas de 2000€.");
+	alert("Hay "+ n +" empleados que cobran entre 1000€-2000€ y "+
+                (5-n) +" que cobran mas de 2000€.");
 	alert("En total se gasta en personal: "+ total +"€.");
-}
-function contar(sueldo,  n) {
-	if (sueldo<=2000) {return n+1;
-	}else{return n;}
 }
--- public_html/UF1_Sintaxis_i_objectes/02_ControlDeFlux/Ex19/script.js
+++ public_html/UF1_Sintaxis_i_objectes/02_ControlDeFlux/Ex19/script.js
@@ -2,7 +2,17 @@ function myFunction() {
 	var lista1 = [2.5, 5, 8, 10];
 	var lista2 = [6, 7.6, 2];
 	var t1 = sumarArray(lista1);
-	var t2 = sumarArray(lista2);
+        
+        t1=0;
+        for (i=0 ; i<= lista1.length ; i++){
+            t1 = t1+lista1[i];
+        }
+        
+        t2=0;
+        for (i=0 ; i<= lista2.length ; i++){
+            t2 = t2+lista2[i];
+        }
+        
 	if (t1 > t2) {
 		alert("La lista 1 tiene un total mayor que la lista 2.");	
 	} else if (t1 < t2) {
@@ -10,11 +20,4 @@ function myFunction() {
 	} else {
 		alert("Las 2 listas tienen un valor total equivalente.");
 	}
-}
-function sumarArray(lista) {
-	var sum = 0;
-	lista.forEach(
-		function addNumber(value) { sum += value; }
-	);
-	return sum;
 }
